941db67bac87c5ca398f5120aa325370a248edf0
nuprostointriga <nuprostointriga@gmail.com>
2025-03-01 03:11:38 +0300

    rework and implementation  "requestAnimationFrame"

--- main.js
+++ main.js
@@ -1,59 +1,116 @@
-const minutes = 25
-let seconds = minutes * 60
+const MINUTES = 1
+const INITIAL_SECONDS = MINUTES * 60
+let seconds = INITIAL_SECONDS
 
-let stateRun = false
+let isRunning = false
 let globalInterval
 
 const timerDiv = document.getElementById('timer')
 
+const formatTime = time => {
+	return `${Math.floor(time / 60)
+		.toString()
+		.padStart(2, '0')}:${(time % 60).toString().padStart(2, '0')}`
+}
+
 function start() {
-	if (stateRun) {
-		return console.log('function is work')
-	} else {
-		stateRun = true
+	if (isRunning) return
 
-		globalInterval = setInterval(() => {
-			seconds--
+	isRunning = true
 
-			if (seconds < 300) {
-				timerDiv.classList.add('fast')
-			}
+	globalInterval = setInterval(() => {
+		seconds--
 
-			if (seconds === 0) {
-				clearInterval(globalInterval)
-				seconds = minutes * 60
-				stateRun = false
+		if (seconds < 300) {
+			timerDiv.classList.add('fast')
+		}
 
-				return (timerDiv.textContent = '25:00')
-			} else {
-				return (timerDiv.textContent = `${
-					Math.floor(seconds / 60) < 10
-						? `0${Math.floor(seconds / 60)}`
-						: Math.floor(seconds / 60)
-				}:${seconds % 60 < 10 ? `0${seconds % 60}` : seconds % 60}`)
-			}
-		}, 1000)
-	}
+		if (seconds === 0) {
+			clearInterval(globalInterval)
+			seconds = INITIAL_SECONDS
+			isRunning = false
+
+			timerDiv.textContent = formatTime(seconds)
+			return
+		}
+
+		timerDiv.textContent = formatTime(seconds)
+	}, 1000)
 }
 
 function pause() {
 	clearInterval(globalInterval)
-	stateRun = false
-
-	console.log('click pause', seconds)
+	isRunning = false
 }
 
 function reset() {
-	console.log('click reset')
-
 	clearInterval(globalInterval)
 	timerDiv.classList.remove('fast')
-	seconds = minutes * 60
-	stateRun = false
+	seconds = INITIAL_SECONDS
+	isRunning = false
 
-	return (timerDiv.textContent = `${
-		Math.floor(seconds / 60) < 10
-			? `0${Math.floor(seconds / 60)}`
-			: Math.floor(seconds / 60)
-	}:${seconds % 60 < 10 ? `0${seconds % 60}` : seconds % 60}`)
+	timerDiv.textContent = formatTime(seconds)
+}
+
+// Request Animation Frame
+
+const INITIAL_MILLISECONDS = INITIAL_SECONDS * 1000
+let milliseconds = INITIAL_MILLISECONDS
+let startTime
+let endTime
+
+let isStarting = false
+
+const animationTimerDiv = document.getElementById('animationTimer')
+
+const formatedTime = time => {
+	return `${Math.floor(time / 60000)
+		.toString()
+		.padStart(2, '0')}:${Math.floor((time % 60000) / 1000)
+		.toString()
+		.padStart(2, '0')}`
+}
+
+const updateTime = () => {
+	if (isStarting) {
+		let currentTime = performance.now()
+		milliseconds = endTime - currentTime
+
+		animationTimerDiv.textContent = formatedTime(milliseconds)
+
+		if (milliseconds < 300000) {
+			animationTimerDiv.classList.add('fast')
+		}
+
+		if (endTime > currentTime) {
+			requestAnimationFrame(updateTime)
+		} else {
+			isStarting = false
+			animationTimerDiv.classList.remove('fast')
+			animationTimerDiv.textContent = formatedTime(INITIAL_MILLISECONDS)
+			return
+		}
+	}
+}
+
+function frameStart() {
+	if (isStarting) return
+
+	isStarting = true
+
+	startTime = performance.now()
+	endTime = startTime + milliseconds
+
+	requestAnimationFrame(updateTime)
+}
+
+function framePause() {
+	isStarting = false
+}
+
+function frameReset() {
+	isStarting = false
+	animationTimerDiv.classList.remove('fast')
+	animationTimerDiv.textContent = formatedTime(INITIAL_MILLISECONDS)
+	milliseconds = INITIAL_MILLISECONDS
 }
